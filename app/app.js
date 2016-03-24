1e9b3567a23c955afbae144b6463d7a8699dbddf
John Keisker <jkeisker@me.com>
2016-03-23 20:22:11 -0500

    styling commits

--- app/app.js
+++ app/app.js
@@ -1,21 +1,26 @@
-setInterval(function() {
+setInterval(() => {
+  let time = new Date();
   console.log(new Date());
-}, 1000)
+
+  let hours = padNumber(time.getHours());
+  let minutes = padNumber(time.getMinutes());
+  let seconds = padNumber(time.getSeconds());
+  let percentMinute =time.getSeconds()/ 60;
+  let totalWidth = 73;
+  console.log(percentMinute);
+
+  document.querySelector("#timerBar").style.width = (percentMinute * totalWidth) + "px";
+
+  document.querySelector("#clock").innerHTML = `${hours}:${minutes}:${seconds}`;
+
+
+}, 1000);
 
 function padNumber(num) {
-  if (num.length < 2) return "0" + num;
-  return num;
-}
-
-function displayTime() {
-  var clock = new Date();
-  h = clock.getHours();
-  m = clock.getMinutes();
-  s = clock.getSeconds();
-  if (h <= 9) h = "0" + h;
-  if (m <= 9) m = "0" + m;
-  if (s <= 9) s = "0" + s;
-
-  var time = h + ':' + m + ':' + s;
-
+  if (num <10) {
+    return "0" + String(num);
+  }
+  else {
+    return String(num);
+  }
 }
